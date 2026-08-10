37b0e068d7104f654ed8de2ac6e1bee94cbb8538
familyoffice0909 <family.office0909@gmail.com>
2026-08-10 13:08:08 -0400

    fix(release): align v3.4.4 engine version metadata

--- Config.js
+++ Config.js
@@ -10,7 +10,7 @@ const FO_CONFIG = {
   },
 
   ENGINE_NAME: 'Family Office CIO Orchestrator',
-  ENGINE_VERSION: 'v3.4.0',
+  ENGINE_VERSION: 'v3.4.4',
 
   get LEDGER_SPREADSHEET_ID() {
     return foGetRuntimeLedgerSpreadsheetId_();
